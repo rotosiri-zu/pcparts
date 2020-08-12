709f301c5e8b01f1e914e1d2e40025dbcb517df8
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-08-12 19:29:48 +0900

    postモデル追加とカラムを追加してマイグレーション実行

---
+++ db/migrate/20200812100708_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.string :image, null: false
+      t.string :genre, null: false
+      t.integer :price, null: false
+      t.text :text, null: false
+      t.timestamps
+    end
+  end
+end
