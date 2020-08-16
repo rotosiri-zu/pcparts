9e91792ed32aea5e7220cfe27f40565a6670c4cd
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-08-16 19:04:02 +0900

    ルーティングにresources :postsのコード追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-
+  resources :posts
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
