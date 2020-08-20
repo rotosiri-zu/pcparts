0bc6995a8e4922335d73577c652380d3596e87ab
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-08-20 15:36:13 +0900

    画像投稿機能の追加

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ApplicationRecord
   belongs_to :post
-
+  mount_uploader :src, ImageUploader
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
