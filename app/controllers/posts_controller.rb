5a7b0c30b94c462d64dc246b6dc35f5bf444e9cd
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-08-29 21:05:44 +0900

    editの投稿テキストを更新するに変更、キャンセルボタンの追加、destroyアクションの追加、インデントの調整、詳細ページに編集、削除リンク追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_posts, only: %i[show]
+  before_action :set_posts, only: %i[show edit update destroy]
 
   def index
     @posts = Post.all
@@ -19,17 +19,33 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def edit
-    end
-    
-    def update
-    end
+  def edit
   end
   
+  def update
+    if @posts.user_id == current_user.id
+      @posts.update(set_posts)
+      if @posts.save
+        flash[:success] = 'アイテムを更新しました'
+        redirect_to root_path
+      else
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    if @posts.user_id == current_user.id
+      flash[:success] = 'アイテムを削除しました'
+      @posts.destroy
+      redirect_to root_path
+    end  
+  end  
   private
   
   def post_params
