dafa52a384550711a1f908b02a9ffdc5eed6e631
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-08-30 21:56:58 +0900

    ルーティングにsearchを追加、

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   resources :posts do 
     collection do
       get 'search'
-    end
+    end  
+  end
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
