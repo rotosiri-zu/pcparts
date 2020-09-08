a43fef9c9ab8c79e13e941658b275d37f60d7b38
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-09-08 21:30:18 +0900

    commentsのテーブル追加

--- db/migrate/20200907114025_create_comments.rb
+++ db/migrate/20200907114025_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+      t.string :rate
+      t.text :content
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
