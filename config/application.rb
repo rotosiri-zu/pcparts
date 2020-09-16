e6a773ca70c6ad667f87887cbc29418c2c081528
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-09-16 22:42:52 +0900

    time_zoneTokyoからAsia/Tokyoに変更、全角スペースを半角スペースに変更、口コミ一覧に投稿した日付けと時間を表示するようにした。

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ module Pcparts
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.time_zone = 'Tokyo'
+    config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
 
-    #　以下の記述を追記する(設定必須)
+    # 以下の記述を追記する(設定必須)
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
 
