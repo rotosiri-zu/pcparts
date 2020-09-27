ea0db0a86b2e8ded6a3d120f96c133398c65187d
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-09-27 21:58:20 +0900

    ()に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,8 +18,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       # credentialsで管理する場合
-      aws_access_key_id: Rails.application.credentials.dig[:aws, :access_key_id],
-      aws_secret_access_key: Rails.application.credentials.dig[:aws, :secret_access_key],
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
       # 環境変数で管理する場合
       # aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
