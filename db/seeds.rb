6667892a3059d306becc8e95a39020bc8ae903b0
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-10-02 00:20:19 +0900

    csvファイルの作成、userモデルにバリデーションを記載、seedにsvcを反映

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+CSV.foreach('db/category.csv') do |row|
+  Category.create(:id => row[0], :name => row[1], :ancestry => row[2])
+end
+
+Post.create!(
+  title: 'テスト',
+  genre: 'マザーボード',
+  price: 35527,
+  text: 'テスト',
+  image: File.open('./app/assets/images/0192876671627_A01.jpg')
+)
+
