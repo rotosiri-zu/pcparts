b2eb260aac4dbebc33b9e201cf78060a54c43155
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-10-17 20:08:39 +0900

    category_idのバリテーション削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates  :title, presence: true
-  validates  :category_id, presence: true
   validates  :price, presence: true
   validates  :text, presence: true
   validates  :image, presence: true
