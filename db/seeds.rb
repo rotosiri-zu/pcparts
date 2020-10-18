632fc524656231d6374b508bd3e6770cf08825d8
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-10-18 22:08:28 +0900

    csvとseedのid項目削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 require "csv"
 
 CSV.foreach('db/category.csv') do |row|
-  Category.create(:id => row[0], :name => row[1], :ancestry => row[2])
+  Category.create(:name => row[0], :ancestry => row[1])
 end
   
 Post.create!(
