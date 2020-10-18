8feeaa4c8e55e925c73b11164ff12cc5185d751d
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-10-18 21:58:50 +0900

    postsテーブルにcategoryカラムにreferences型で追加変更

--- db/migrate/20200812100708_create_posts.rb
+++ db/migrate/20200812100708_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.string :title, null: false
       t.integer :price, null: false
       t.text :text, null: false
-      t.integer :category_id
+      t.references :category
       t.timestamps
     end
   end
