fbc96454664ea83e888bffcc9d48e1563080da86
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-10-18 22:39:06 +0900

    seedのcategoryの項目変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 require "csv"
 
 CSV.foreach('db/category.csv') do |row|
-  Category.create(:id => row[0], :name => row[1], :ancestry => row[2])
+  Category.create(name: row['name'], ancestry: row['ancestry'])
 end
   
 Post.create!(
