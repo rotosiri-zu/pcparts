35b8d38d47dda0d576eb314a3c21423feff6ab42
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-10-20 23:41:51 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ CSV.foreach('db/category.csv') do |row|
 end
   
 Post.all.each do |post|
-  post.categorys.create_author(
+  post.category.create_author(
     [
       {
         title: 'Core i9-9900K BOX品',
