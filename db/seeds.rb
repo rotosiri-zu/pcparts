ede1d4a67ef47a55a8d577962ead69c53a136869
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-10-20 19:24:06 +0900

    変更を解除

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 require "csv"
 
-CSV.foreach('db/category.csv', headers: true) do |row|
-  Category.create(name: row['name'], ancestry: row['ancestry'])
+CSV.foreach('db/category.csv') do |row|
+  Category.create(id: row[0] name: row[1], ancestry: row[2])
 end
   
 # Post.create!(
