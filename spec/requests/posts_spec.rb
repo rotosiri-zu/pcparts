1362ad76d57ae0d9bfdfed2c6f255db89f602f88
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-11-11 23:04:24 +0900

    request specのテストのテストコード追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  
+  describe "GET#index" do
+    before do
+      get posts_path
+    end
+    it "投稿したPCパーツが一覧で表示される" do
+      expect(response).to have_http_status(200)
+    end
+  end
 end
