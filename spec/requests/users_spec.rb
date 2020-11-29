32c2b3796d6a1af415aa0509f970285ec3804063
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-11-29 23:07:41 +0900

    comments_specのテストコードを一時コメントアウト、users_specにshowアクションのテストを追加

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  let(:user) { FactoryBot.create(:user)}
+
+  describe 'GET #show' do
+    it 'リクエストが成功すること' do
+      get user_path user
+      expect(response.status).to eq 200
+    end
+  end
+end
