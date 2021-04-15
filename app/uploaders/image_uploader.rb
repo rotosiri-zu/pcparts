e6343ed1fe1e92a490f5f7e5cf56666595dae6a9
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2021-04-15 20:23:49 +0900

    postsテーブルにurlのカラムを追加、imageカラムをimage_urlカラムに変更, postsモデルとビューのimageをimage_urlに変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [200, 200]
 
   # Choose what kind of storage to use for this uploader:
   # developmentとtest以外はS3を使用
--- db/migrate/20200812100708_create_posts.rb
+++ db/migrate/20200812100708_create_posts.rb
@@ -1,10 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :image, null: false
       t.string :title, null: false
+      t.text :url
+      t.string :image_url, null: false
       t.integer :price, null: false
       t.text :text, null: false
+      t.string :genre_id
       t.references :category
       t.timestamps
     end
