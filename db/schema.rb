bd0c5f41f5e201f219e16e38d182494e62f6c924
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2021-04-24 22:20:18 +0900

    commentテーブルにpictureカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -27,13 +27,12 @@ ActiveRecord::Schema.define(version: 2021_04_04_140545) do
     t.string "title", null: false
     t.float "rate", null: false
     t.text "content", null: false
+    t.string "picture"
     t.bigint "user_id"
     t.bigint "post_id"
-    t.bigint "rakuten_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["rakuten_id"], name: "index_comments_on_rakuten_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
