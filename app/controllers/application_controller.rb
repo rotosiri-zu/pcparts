85234e414ea6b51a6bf500afe95a77171eb65d67
rotosiri-zu <hongjitufang25@gmail.com>
2021-06-14 01:01:16 +0900

    インデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,27 +8,27 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
-  end
+    def set_current_user
+      @current_user = User.find_by(id: session[:user_id])
+    end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
+    end
 
-  def set_search
-    @pagenumber = 5
-    @q = Post.all.ransack(params[:q])
-    @posts = @q.result(distinct: true).order(id: "DESC").page(params[:page]).per(@pagenumber)
-  end
+    def set_search
+      @pagenumber = 5
+      @q = Post.all.ransack(params[:q])
+      @posts = @q.result(distinct: true).order(id: "DESC").page(params[:page]).per(@pagenumber)
+    end
 
-  def set_category_list
-    @category_parent_array = Category.where(ancestry: nil)
-  end
+    def set_category_list
+      @category_parent_array = Category.where(ancestry: nil)
+    end
 
-  def record_not_found
-    flash[:error] = "ご指定のページが見つかりません"
-    redirect_back(fallback_location: root_path)
-  end
+    def record_not_found
+      flash[:error] = "ご指定のページが見つかりません"
+      redirect_back(fallback_location: root_path)
+    end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,12 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: :show
-
   def show
     @posts = @category.posts.order(created_at: :desc).all
   end
 
   private
 
-  def set_category
-    @category = Category.find(params[:id])
-  end
+    def set_category
+      @category = Category.find(params[:id])
+    end
 end
